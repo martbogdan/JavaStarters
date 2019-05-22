d2dad6717b20411e52137ed01af2cb451a8f38f8
martbogdan <mbogdan0123@gmail.com>
2019-05-22 22:41:33 +0300

    Home task

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main (String[] args){
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number of task what you would like to do tomorrow. \n" +
+                "NOTE: You can write only 1, 2 or 3");
+        int numOfTask = scan.nextInt();
+        scan.close();
+        if (numOfTask==1){
+            System.out.println("First task");
+        }else if (numOfTask==2){
+            System.out.println("Second task");
+        }else if (numOfTask==3){
+            System.out.println("Third task");
+        }else {
+            System.out.println("You wrote a wrong number, Task with this number is empty.\nPlease, try again.");
+        }
+
+
     }
 }
