068488049a7c2d637283d7081a35f46f751b3f7b
martbogdan <mbogdan0123@gmail.com>
2019-05-23 12:13:13 +0300

    Changed second task

--- src/Hello.java
+++ src/Hello.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Hello {
     public static void main (String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number of task what you would like to do tomorrow. \n" +
+        System.out.println("Enter a number of task what you would like to do tomorrow.\n" +
                 "NOTE: You can write only 1, 2 or 3");
         int numOfTask = scan.nextInt();
         scan.close();
         if (numOfTask==1){
             System.out.println("You have to install all programs you need");
         }else if (numOfTask==2){
-            System.out.println("Second task");
+            System.out.println("You have to create GIT repository and branch 'dev' on it");
         }else if (numOfTask==3){
             System.out.println("Third task");
         }else {
