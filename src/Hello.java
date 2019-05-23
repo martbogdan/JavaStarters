a79baa6dfb3aa507d2798cc53bd29d660846e5d1
martbogdan <mbogdan0123@gmail.com>
2019-05-23 23:15:11 +0300

    Added block "try catch"

--- src/Hello.java
+++ src/Hello.java
@@ -3,18 +3,22 @@ import java.util.Scanner;
 public class Hello {
     public static void main (String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number of task what you would like to do tomorrow.\n" +
-                "NOTE: You can write only 1, 2 or 3");
-        int numOfTask = scan.nextInt();
-        scan.close();
-        if (numOfTask==1){
-            System.out.println("You have to install all programs you need");
-        }else if (numOfTask==2){
-            System.out.println("You have to create GIT repository and branch 'dev' on it");
-        }else if (numOfTask==3){
-            System.out.println("You have to develop Java program");
-        }else {
-            System.out.println("You wrote a wrong number, Task with this number is empty.\nPlease, try again.");
+        try {
+            System.out.println("Enter a number of task what you would like to do tomorrow.\n" +
+                    "NOTE: You can write only 1, 2 or 3");
+            int numOfTask = scan.nextInt();
+            scan.close();
+            if (numOfTask == 1) {
+                System.out.println("You have to install all programs you need");
+            } else if (numOfTask == 2) {
+                System.out.println("You have to create GIT repository and branch 'dev' on it");
+            } else if (numOfTask == 3) {
+                System.out.println("You have to develop Java program");
+            } else {
+                System.out.println("You wrote a wrong number, Task with this number is empty.\nPlease, try again.");
+            }
+        } catch (Exception eRR) {
+            System.out.println("ERROR: You can't use any symbols, only numbers");
         }
     }
 }
